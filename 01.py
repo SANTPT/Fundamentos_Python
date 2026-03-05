4e4c4c444c946f4bde10a699bfc7701921bf3add
SANTPT <santiago.patino261@comunidadunir.net>
2026-03-05 11:48:49 +0100

    first commit

---
+++ 01.py
@@ -0,0 +1,60 @@
+# print("Hola, mundo")
+# print("Hola, mundo")
+# print(42)
+# print(9.4)
+# print(True, False)
+# print(["a", "b"])
+# print({"name": "John", "age": 21})
+
+
+# customer_name = "John"
+# print(customer_name)
+
+
+# def mul(a, factor=2):
+#     return a * factor
+
+
+# print(mul(18))
+
+
+# operandos = [1, 3, 2, 5, 6, 9]
+# print(operandos[::-1])
+# print(operandos[2:])
+# print(operandos[:5])
+# print(operandos[-2])
+# print(operandos[2:-2])
+
+
+
+
+def is_greater(a, b):
+    if a > b:
+        return True
+    else:
+        return False
+
+
+
+
+# print(is_greater(2, 1))
+# print(is_greater(2, 11))
+# print(is_greater(2, 2))
+
+
+
+
+
+
+
+
+#operandos = [1, 3, 2, 5, 6, 9]
+
+
+#for idx, operando in enumerate(operandos):
+#   print(idx, operando)
+
+
+
+
+
